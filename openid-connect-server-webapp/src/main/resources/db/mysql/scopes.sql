397a0777705ea261e4566d8e4d2c8e770747bcc1
agcopley <andrewgcopley@gmail.com>
2022-11-24 14:21:03 -0300

    Update scopes.sql
    Update sql

--- openid-connect-server-webapp/src/main/resources/db/mysql/scopes.sql
+++ openid-connect-server-webapp/src/main/resources/db/mysql/scopes.sql
@@ -10,7 +10,7 @@ START TRANSACTION;
 -- Insert scope information into the temporary tables.
 -- 
 
-INSERT INTO system_scope_TEMP (scope, description, icon, restricted, default_scope) VALUES
+INSERT INTO system_scope (scope, description, icon, restricted, default_scope) VALUES
   ('openid', 'log in using your identity', 'user', false, true),
   ('profile', 'basic profile information', 'list-alt', false, true),
   ('email', 'email address', 'envelope', false, true),
@@ -18,14 +18,6 @@ INSERT INTO system_scope_TEMP (scope, description, icon, restricted, default_sco
   ('phone', 'telephone number', 'bell', false, true),
   ('offline_access', 'offline access', 'time', false, false);
   
---
--- Merge the temporary scopes safely into the database. This is a two-step process to keep scopes from being created on every startup with a persistent store.
---
-
-INSERT INTO system_scope (scope, description, icon, restricted, default_scope, structured, structured_param_description) 
-  SELECT scope, description, icon, restricted, default_scope, structured, structured_param_description FROM system_scope_TEMP
-  ON DUPLICATE KEY UPDATE system_scope.scope = system_scope.scope;
-
 COMMIT;
 
 SET AUTOCOMMIT = 1;
