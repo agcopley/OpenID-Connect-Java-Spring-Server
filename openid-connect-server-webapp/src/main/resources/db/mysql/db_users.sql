67d08a5f4adf4c65e6efd9ab41179235ff8bf213
agcopley <andrewgcopley@gmail.com>
2022-11-24 15:22:00 -0300

    Update db_users.sql
    Update SQL

--- openid-connect-server-webapp/src/main/resources/db/mysql/db_users.sql
+++ openid-connect-server-webapp/src/main/resources/db/mysql/db_users.sql
@@ -1,6 +1,9 @@
-CREATE USER 'oicuser'@'localhost' IDENTIFIED WITH mysql_native_password 'oicuser';
-CREATE USER 'oicuser'@'%' IDENTIFIED WITH mysql_native_password 'oicuser';
+CREATE USER 'oicuser'@'localhost' IDENTIFIED BY 'oicuser';
+CREATE USER 'oicuser'@'%' IDENTIFIED BY 'oicuser';
 
 GRANT ALL ON *.* TO 'oicuser'@'localhost';
 GRANT ALL ON *.* TO 'oicuser'@'%';
 FLUSH PRIVILEGES;
+
+
+ALTER USER 'oicuser'@'localhost' IDENTIFIED WITH mysql_native_password BY 'oicuser'
